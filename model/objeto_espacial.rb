51dd744a0f1a550e4f6c13306f23d726993882ec
Emiliano <emilianoveppo@gmail.com>
2016-06-25 15:33:08 -0300

    Se genera un objeto espacial con masa y vida

---
+++ model/objeto_espacial.rb
@@ -0,0 +1,10 @@
+class ObjetoEspacial
+
+	attr_accessor :vida, :masa, :objeto_vivo, :efecto_choque
+
+	 def initialize(*args)
+	 	@vida = 100
+	 	@masa = 100
+	 end
+	
+end
---
+++ spec/objeto_espacial_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec' 
+require 'spec_helper'
+require_relative '../model/objeto_espacial'
+
+describe 'ObjetoEspacial' do
+
+  it 'Se inicia el Objeto Espacial con Vida 100' do
+    objetoEspacial = ObjetoEspacial.new
+    expect(objetoEspacial.vida).to eq 100
+  end
+
+end
