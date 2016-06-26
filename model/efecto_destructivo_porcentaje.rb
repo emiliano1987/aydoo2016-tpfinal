2485e3db2043f27543cb04b20135092e3f77c230
Emiliano <emilianoveppo@gmail.com>
2016-06-26 15:28:13 -0300

    Se modifica el efecto destructivo por porcentaje ya que existia codigo innecesario

--- model/efecto_destructivo_porcentaje.rb
+++ model/efecto_destructivo_porcentaje.rb
@@ -3,11 +3,7 @@ require_relative '../model/objeto_espacial'
 class EfectoDestructivoPorcentaje
 
 	  def impacto(objeto_espacial_genera_impacto, objeto_del_espacio_impactado, porcentaje_de_impacto)
-	  	if (porcentaje_de_impacto == 100)
-	  		objeto_espacial_genera_impacto.vida = 0
-	  	else
     		disminuye_vida = (objeto_espacial_genera_impacto.vida * porcentaje_de_impacto / 100)
 			objeto_espacial_genera_impacto.vida -= disminuye_vida
-		end
 	  end
 end
