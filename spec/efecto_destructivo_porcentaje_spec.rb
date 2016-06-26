063bf7a8f58ec978774d31bc6f9399a4070dd190
Emiliano <emilianoveppo@gmail.com>
2016-06-26 02:38:02 -0300

    Se genera nuevos test para el tipo de efecto destructivo porcentual

--- spec/efecto_destructivo_porcentaje_spec.rb
+++ spec/efecto_destructivo_porcentaje_spec.rb
@@ -4,7 +4,7 @@ require_relative '../model/efecto_destructivo_porcentaje'
 
 describe 'EfectoDestructivoPorcentaje' do
 
-  it 'El impacto entre dos objetos no modifica sus atributos' do
+  it 'El impacto entre dos objetos deja sin vida a uno' do
     asteroide = Asteroide.new
     bomba = Bomba.new
     efecto = EfectoDestructivoPorcentaje.new
@@ -12,4 +12,27 @@ describe 'EfectoDestructivoPorcentaje' do
     expect(asteroide.vida).to eq 0
   end
 
+  it 'El impacto entre dos objetos no modifica la vida del objeto impactado' do
+    nave = Nave.new
+    asteroide = Asteroide.new
+    efecto = EfectoDestructivoPorcentaje.new
+    efecto.impacto(nave,asteroide,100)
+    expect(nave.vida).to eq 0
+  end
+
+  it 'El impacto entre dos objetos verifica que el objeto que genera el impacto sigue con vida' do
+    estrella = Estrella.new
+    misil = Misil.new
+    efecto = EfectoDestructivoPorcentaje.new
+    efecto.impacto(estrella,misil,50)
+    expect(estrella.vida).to eq 50
+  end
+
+  it 'El impacto entre dos objetos verifica que el objeto que genera el impacto esta sin vida' do
+    estrella = Estrella.new
+    misil = Misil.new
+    efecto = EfectoDestructivoPorcentaje.new
+    efecto.impacto(estrella,misil,100)
+    expect(estrella.objeto_esta_vivo).to eq false
+  end
 end
