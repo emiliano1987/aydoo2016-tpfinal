4e95a171ca6aeceb0a71e89bc5a95bf157bb4e59
Emiliano <emilianoveppo@gmail.com>
2016-06-26 01:13:15 -0300

    Se agrega test para objeto Nave

--- spec/nave_spec.rb
+++ spec/nave_spec.rb
@@ -13,4 +13,9 @@ describe 'Nave' do
     nave = Nave.new
     expect(nave.vida).to eq 100
   end
+
+   it 'La nave es un objeto vivo cuando se inicia' do
+    nave = Nave.new
+    expect(nave.objeto_esta_vivo).to eq true
+  end
 end
