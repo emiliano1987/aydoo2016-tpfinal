aa38baf4a96abe220548c8b09a0d3d5e736b381a
Emiliano <emilianoveppo@gmail.com>
2016-06-26 01:23:50 -0300

    Se agrega test para la clase Misil

--- spec/misil_spec.rb
+++ spec/misil_spec.rb
@@ -24,4 +24,14 @@ describe 'Misil' do
     expect(misil.objeto_esta_vivo).to eq false
   end
 
+  it 'El misil se inicializa con masa de 1 ' do
+    misil = Misil.new 2, 1
+    expect(misil.masa).to eq 1
+  end
+
+  it 'El misil se inicializa con vida de 0 ' do
+    misil = Misil.new 0,0
+    expect(misil.vida).to eq 0
+  end
+
 end
