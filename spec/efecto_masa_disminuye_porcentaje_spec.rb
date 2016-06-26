c8cdd310a1cc0ec953c9aaab0c0e3f37518ecaf5
Emiliano <emilianoveppo@gmail.com>
2016-06-26 15:44:04 -0300

    Se agrega nuevo test para el efecto que disminuye la masa del objeto

--- spec/efecto_masa_disminuye_porcentaje_spec.rb
+++ spec/efecto_masa_disminuye_porcentaje_spec.rb
@@ -4,7 +4,7 @@ require_relative '../model/efecto_masa_disminuye_porcentaje'
 
 describe 'EfectoMasaDisminuyePorcentaje' do
 
-  it 'El impacto entre dos objetos deja sin masa al objeto impactado' do
+  it 'El impacto entre dos objetos deja sin masa al objeto que impacta ' do
     asteroide = Asteroide.new
     bomba = Bomba.new
     efecto = EfectoMasaDisminuyePorcentaje.new
@@ -20,4 +20,11 @@ describe 'EfectoMasaDisminuyePorcentaje' do
     expect(asteroide.objeto_esta_vivo).to eq false
   end
 
+  it 'El impacto entre dos objetos mantiene con vida al objeto que impacta ' do
+    asteroide = Asteroide.new 300, 15
+    bomba = Bomba.new 456, 10
+    efecto = EfectoMasaDisminuyePorcentaje.new
+    efecto.impacto(asteroide,bomba,14)
+    expect(asteroide.objeto_esta_vivo).to eq true
+  end
 end
