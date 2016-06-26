83835e3bd42d926be35ebdce684ac36482cf4551
Emiliano <emilianoveppo@gmail.com>
2016-06-26 15:33:44 -0300

    Se agrega nuevo test para el tipo de efecto constructivo en su masa

--- spec/efecto_masa_aumento_porcentaje_spec.rb
+++ spec/efecto_masa_aumento_porcentaje_spec.rb
@@ -12,5 +12,12 @@ describe 'EfectoMasaAumentaPorcentaje' do
     expect(nave.masa).to eq 55
   end
 
+  it 'El impacto entre dos objetos verifica que el objeto esta con vida' do
+    misil = Misil.new 50, 1
+    asteroide = Asteroide.new 1000, 1000
+    efecto = EfectoMasaAumentaPorcentaje.new
+    efecto.impacto(misil,asteroide,50)
+    expect(misil.masa).to eq 501
+  end
 
 end
