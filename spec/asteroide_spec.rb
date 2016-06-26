9b639f78c8ace9b95dbd392d89a476fe3db42c41
Emiliano <emilianoveppo@gmail.com>
2016-06-26 01:42:22 -0300

    Se genera nuevo test para la clase Asteroide

--- spec/asteroide_spec.rb
+++ spec/asteroide_spec.rb
@@ -5,13 +5,32 @@ require_relative '../model/asteroide'
 describe 'Asteroide' do
 
   it 'Se inicia Asteroide con Vida 100' do
-    bomba = Asteroide.new
-    expect(bomba.vida).to eq 100
+    asteroide = Asteroide.new
+    expect(asteroide.vida).to eq 100
   end
 
   it 'Se inicia Asteroide con Masa 100' do
-    bomba = Asteroide.new
-    expect(bomba.vida).to eq 100
+    asteroide = Asteroide.new
+    expect(asteroide.vida).to eq 100
+  end
+
+  it 'El objeto Asteroide es un objeto muerto cuando se inicia' do
+    asteroide = Asteroide.new 0,0
+    expect(asteroide.objeto_esta_vivo).to eq false
+  end
+
+  it 'El objeto Asteroide se inicializa con masa de 1 ' do
+    asteroide = Asteroide.new 2, 1
+    expect(asteroide.masa).to eq 1
+  end
+
+  it 'El objeto Asteroide se inicializa con vida de 0 ' do
+    asteroide = Asteroide.new 0,0
+    expect(asteroide.vida).to eq 0
+  end
+
+  it 'El ingreso de parametros es incorrecto' do
+ 	expect{asteroide = Asteroide.new 0,1,0}.to raise_error(ArgumentError)  
   end
 
 end
