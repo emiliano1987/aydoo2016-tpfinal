36d94ef6ffa795177d871b584ba92df317134ba8
Emiliano <emilianoveppo@gmail.com>
2016-06-26 02:09:30 -0300

    Se genera clase con tipo de efecto nulo con el cual dos objetos si impactan no modifican sus valores

---
+++ model/efecto_nulo.rb
@@ -0,0 +1,9 @@
+
+class EfectoNulo
+
+	  def impacto(objeto_espacial_genera_impacto, objeto_del_espacio_impactado)
+=begin
+Este metodo no modifica ningun objeto ya que su invocacion no provoca cambio en masa o vida
+=end
+	  end
+end
---
+++ spec/efecto_nulo_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec' 
+require 'spec_helper'
+require_relative '../model/efecto_nulo'
+
+describe 'EfectoNulo' do
+
+  it 'El impacto entre dos objetos no modifica sus atributos' do
+    asteroide = Asteroide.new
+    bomba = Bomba.new
+    efecto = EfectoNulo.new
+    efecto.impacto(asteroide,bomba)
+    expect(bomba.vida).to eq 100
+  end
+
+end
