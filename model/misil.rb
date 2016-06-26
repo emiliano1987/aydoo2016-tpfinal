8ea0b1062acb4f9311ae80fbb3e1c81aabdfbc8a
Emiliano <emilianoveppo@gmail.com>
2016-06-26 19:41:12 -0300

    Se agregan modificaciones en el objeto espacial del tipo Misil que permitan el impacto entre diferentes objetos

--- model/misil.rb
+++ model/misil.rb
@@ -1,8 +1,20 @@
 require_relative '../model/objeto_espacial'
+require_relative '../model/efecto_destructivo_unidades'
+require_relative '../model/efecto_nulo'
 
 class Misil < ObjetoEspacial
 
 	def initialize (*args)
 		super(*args)
+    	generar_efectos_segun_objetos(efecto_choque)
+    end
+	
+	private
+	def generar_efectos_segun_objetos (mapa_choque)
+		mapa_choque[Nave] = EfectoDestructivoUnidades.new 100
+	    mapa_choque[Misil] = EfectoDestructivoUnidades.new 80
+	    mapa_choque[Bomba] = EfectoNulo.new
+	    mapa_choque[Asteroide] = EfectoNulo.new
+		mapa_choque[Estrella] = EfectoNulo.new
 	end
 end
--- spec/misil_spec.rb
+++ spec/misil_spec.rb
@@ -4,38 +4,48 @@ require_relative '../model/misil'
 
 describe 'Misil' do
 
-  it 'Se inicia Misil con Vida 100' do
+  it 'deberia contener 100 unidades de vida al crear el Misil' do
     misil = Misil.new
     expect(misil.vida).to eq 100
   end
 
-  it 'Se inicia Misil con Masa 100' do
+  it 'deberia contener 100 unidades de masa al crear el Misil' do
     misil = Misil.new
-    expect(misil.vida).to eq 100
+    expect(misil.masa).to eq 100
   end
 
-  it 'La misil es un objeto vivo cuando se inicia' do
+  it 'deberia ser un objeto espacial vivo al crear el Misil con 1 unidad de vida y 2 de masa' do
     misil = Misil.new 1, 2
     expect(misil.objeto_esta_vivo).to eq true
   end
 
-  it 'La misil es un objeto muerto cuando se inicia' do
+  it 'deberia ser un objeto espacial sin vida al crear el Misil con 0 unidades de vida y 0 de masa' do
     misil = Misil.new 0,0
     expect(misil.objeto_esta_vivo).to eq false
   end
 
-  it 'El misil se inicializa con masa de 1 ' do
+  it 'deberia ser un Misil con 1 unidad de masa' do
     misil = Misil.new 2, 1
     expect(misil.masa).to eq 1
   end
 
-  it 'El misil se inicializa con vida de 0 ' do
-    misil = Misil.new 0,0
+  it 'deberia ser un objeto espacial sin vida al crear el Misil con 0 unidades de vida' do
+    misil = Misil.new 0,5000
     expect(misil.vida).to eq 0
   end
 
-  it 'El ingreso de parametros es incorrecto' do
- 	expect{misil = Misil.new 0,1,0}.to raise_error(ArgumentError)  
+  it 'deberia no permitir el ingreso de mas de dos parametros al crear la Nave' do
+ 	  expect{misil = Misil.new 100,501,800,11}.to raise_error(ArgumentError)  
   end
 
+  it 'deberia contener 325 unidades de vida al impactar con otro objeto espacial del tipo Nave' do
+    misil = Misil.new 425, 800
+    nave = Nave.new 311, 77
+    
+
+    misil.impacta_con_objeto nave
+
+    expect(misil.vida).to eq 325
+    expect(misil.objeto_esta_vivo).to eq true
+  end
 end
