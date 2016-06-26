dcf866f2b7519999fe2a617bc2fce18ab864ef6f
Emiliano <emilianoveppo@gmail.com>
2016-06-26 00:03:43 -0300

    Se genera el objeto espacial del tipo Nave con masa 100

--- spec/nave_spec.rb
+++ spec/nave_spec.rb
@@ -5,7 +5,12 @@ require_relative '../model/nave'
 describe 'Nave' do
 
   it 'Se inicia Nave con Vida 100' do
-    objetoEspacial = Nave.new
-    expect(objetoEspacial.vida).to eq 100
+    nave = Nave.new
+    expect(nave.vida).to eq 100
+  end
+
+  it 'Se inicia Nave con Masa 100' do
+    nave = Nave.new
+    expect(nave.vida).to eq 100
   end
 end
