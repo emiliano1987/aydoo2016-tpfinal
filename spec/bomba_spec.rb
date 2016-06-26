7d61014f5242f0aab419a4b4ed92758d82a1af29
Emiliano <emilianoveppo@gmail.com>
2016-06-26 01:34:58 -0300

    Se agrega test para la clase bomba

--- spec/bomba_spec.rb
+++ spec/bomba_spec.rb
@@ -14,5 +14,19 @@ describe 'Bomba' do
     expect(bomba.vida).to eq 100
   end
 
+  it 'La Bomba es un objeto muerto cuando se inicia' do
+    misil = Bomba.new 0,0
+    expect(misil.objeto_esta_vivo).to eq false
+  end
+
+  it 'El misil se inicializa con masa de 1 ' do
+    misil = Bomba.new 2, 1
+    expect(misil.masa).to eq 1
+  end
+
+  it 'El misil se inicializa con vida de 0 ' do
+    misil = Bomba.new 0,0
+    expect(misil.vida).to eq 0
+  end
 
 end
