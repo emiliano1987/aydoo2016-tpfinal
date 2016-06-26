e396af5ed79db16a8cb41f4c7f030c915e257331
Emiliano <emilianoveppo@gmail.com>
2016-06-26 20:09:56 -0300

    Se modifica la clase bomba para interactuar con otros objetos espaciales

--- model/bomba.rb
+++ model/bomba.rb
@@ -1,8 +1,23 @@
 require_relative '../model/objeto_espacial'
+require_relative '../model/objeto_espacial'
+require_relative '../model/efecto_constructivo'
+require_relative '../model/efecto_destructivo_unidades'
+require_relative '../model/efecto_destructivo_porcentaje'
 
 class Bomba < ObjetoEspacial
 
 	def initialize (*args)
 		super(*args)
+    	generar_efectos_segun_objetos (efecto_choque)
+    end
+	
+	private
+	def generar_efectos_segun_objetos (mapa_choque)
+		mapa_choque[Nave] = EfectoDestructivoUnidades.new 100
+	    mapa_choque[Misil] = EfectoDestructivoPorcentaje.new 50
+	    mapa_choque[Bomba] = EfectoDestructivoUnidades.new 100
+	    mapa_choque[Asteroide] = EfectoDestructivoPorcentaje.new 100
+		mapa_choque[Estrella] = EfectoDestructivoUnidades.new 100 
 	end
+
 end
--- spec/bomba_spec.rb
+++ spec/bomba_spec.rb
@@ -4,33 +4,43 @@ require_relative '../model/bomba'
 
 describe 'Bomba' do
 
-  it 'Se inicia Bomba con Vida 100' do
+  it 'deberia contener 100 unidades de vida al crear el objeto espacial' do
     bomba = Bomba.new
     expect(bomba.vida).to eq 100
   end
 
-  it 'Se inicia Bomba con Masa 100' do
+  it 'deberia contener 100 unidades de masa al crear el objeto espacial' do
     bomba = Bomba.new
-    expect(bomba.vida).to eq 100
+    expect(bomba.masa).to eq 100
   end
 
-  it 'El objeto Bomba es un objeto vivo cuando se inicia' do
+  it 'deberia ser un objeto espacial vivo al generarse' do
     bomba = Bomba.new 1,1
     expect(bomba.objeto_esta_vivo).to eq true
   end
 
-  it 'El objeto bomba se inicializa con masa de 1 ' do
+  it 'deberia ser un objeto espacial vivo con 1 unidad de masa al generarse' do
     bomba = Bomba.new 2, 1
     expect(bomba.masa).to eq 1
   end
 
-  it 'El objeto bomba se inicializa con vida de 0 ' do
+  it 'deberia ser un objeto espacial con 0 unidades de vida al instanciarse' do
     bomba = Bomba.new 0,0
     expect(bomba.vida).to eq 0
   end
 
-  it 'El ingreso de parametros es incorrecto' do
- 	expect{bomba = Bomba.new 0,1,0}.to raise_error(ArgumentError)  
+  it 'deberia no permitir el ingreso de tres parametros al crear la Bomba' do
+  	expect{bomba = Bomba.new 0,1,0}.to raise_error(ArgumentError)  
+  end
+
+  it 'deberia contener 333 unidades de vida al impactar con otro objeto espacial del tipo Nave' do
+    bomba = Bomba.new 433, 500
+    nave = Nave.new
+
+    bomba.impacta_con_objeto nave
+
+    expect(bomba.vida).to eq 333
+    expect(nave.objeto_esta_vivo).to eq true
   end
 
 end
