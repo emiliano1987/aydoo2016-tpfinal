6a21d601718d56ed567e99090f625680f5649af1
Emiliano <emilianoveppo@gmail.com>
2016-06-26 18:14:11 -0300

    Se agregan test que permiten el impacto entre una Estrella con otros objetos espaciales

--- model/estrella.rb
+++ model/estrella.rb
@@ -1,8 +1,21 @@
 require_relative '../model/objeto_espacial'
+require_relative '../model/efecto_destructivo_porcentaje'
+require_relative '../model/efecto_nulo'
 
 class Estrella < ObjetoEspacial
 
 	def initialize (*args)
 		super(*args)
+    	generar_efectos_segun_objetos (efecto_choque)
+    end
+	
+	private
+	def generar_efectos_segun_objetos (mapa_choque)
+		mapa_choque[Nave] = EfectoDestructivoPorcentaje.new 100
+	    mapa_choque[Misil] = EfectoNulo.new()
+	    mapa_choque[Bomba] = EfectoDestructivoPorcentaje.new 100
+	    mapa_choque[Asteroide] = EfectoDestructivoPorcentaje.new 100
+		mapa_choque[Estrella] = EfectoDestructivoPorcentaje.new 100 
 	end
+
 end
--- spec/estrella_spec.rb
+++ spec/estrella_spec.rb
@@ -4,33 +4,52 @@ require_relative '../model/estrella'
 
 describe 'Estrella' do
 
-  it 'Se inicia Estrella con Vida 100' do
+  it 'deberia contener 100 unidades de vida al crear el objeto espacial' do
     estrella = Estrella.new
     expect(estrella.vida).to eq 100
   end
 
-  it 'Se inicia Estrella con Masa 100' do
+  it 'deberia contener 100 unidades de masa al crear el objeto espacial' do
     estrella = Estrella.new
     expect(estrella.vida).to eq 100
   end
 
-  it 'El objeto Estrella es un objeto muerto cuando se inicia' do
+  it 'deberia ser una Estrella muerta al contener 0 unidades de vida cuando se crear el objeto' do
     estrella = Estrella.new 0,0
     expect(estrella.objeto_esta_vivo).to eq false
   end
 
-  it 'El objeto Estrella se inicializa con masa de 1 ' do
+  it 'deberia contener 1 unidades de masa al crear el objeto espacial' do
     estrella = Estrella.new 2, 1
     expect(estrella.masa).to eq 1
   end
 
-  it 'El objeto Estrella se inicializa con vida de 0 ' do
+  it 'deberia contener 0 unidades de vida al crear el objeto espacial' do
     estrella = Estrella.new 0,0
     expect(estrella.vida).to eq 0
   end
 
-  it 'El ingreso de parametros es incorrecto' do
+  it 'deberia no permitir el ingreso de mas de dos parametros al crear la Estrella' do
   expect{estrella = Estrella.new 0,1,0}.to raise_error(ArgumentError)  
   end
 
+  it 'deberia contener 0 unidades de vida al impactar con otra Estrella' do
+    estrella = Estrella.new 1000, 20
+    nueva_estrella = Estrella.new 1,2
+
+    estrella.impacta_con_objeto nueva_estrella
+
+    expect(estrella.vida).to eq 0
+    expect(estrella.objeto_esta_vivo).to eq false
+  end
+
+  it 'deberia contener 0 unidades de vida al impactar con una Nave' do
+    nave = Nave.new
+    nueva_estrella = Estrella.new 1000,2
+
+    nueva_estrella.impacta_con_objeto nave
+
+    expect(nueva_estrella.vida).to eq 0
+    expect(nueva_estrella.objeto_esta_vivo).to eq false
+  end
 end
