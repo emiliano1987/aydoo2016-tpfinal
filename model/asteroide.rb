f68643f331b774321519c5851ffd3d84439facc9
Emiliano <emilianoveppo@gmail.com>
2016-06-26 20:40:45 -0300

    Se modifica la clase Asteroide para interactuar con otros objetos espaciales

--- model/asteroide.rb
+++ model/asteroide.rb
@@ -1,8 +1,21 @@
 require_relative '../model/objeto_espacial'
+require_relative '../model/efecto_masa_aumento_porcentaje'
+require_relative '../model/efecto_nulo'
 
 class Asteroide < ObjetoEspacial
 
 	def initialize (*args)
 		super(*args)
+    	generar_efectos_segun_objetos (efecto_choque)
+    end
+	
+	private
+	def generar_efectos_segun_objetos (mapa_choque)
+		mapa_choque[Nave] = EfectoMasaAumentaPorcentaje.new 10
+	    mapa_choque[Misil] = EfectoNulo.new
+	    mapa_choque[Bomba] = EfectoNulo.new
+	    mapa_choque[Asteroide] = EfectoNulo.new
+		mapa_choque[Estrella] = EfectoNulo.new 
 	end
+
 end
--- spec/asteroide_spec.rb
+++ spec/asteroide_spec.rb
@@ -4,33 +4,49 @@ require_relative '../model/asteroide'
 
 describe 'Asteroide' do
 
-  it 'Se inicia Asteroide con Vida 100' do
+  it 'deberia contener 100 unidades de vida al crear el Asteroide' do
     asteroide = Asteroide.new
+    
     expect(asteroide.vida).to eq 100
   end
 
-  it 'Se inicia Asteroide con Masa 100' do
+  it 'deberia contener 100 unidades de masa al crear el Asteroide' do
     asteroide = Asteroide.new
-    expect(asteroide.vida).to eq 100
+    
+    expect(asteroide.masa).to eq 100
   end
 
-  it 'El objeto Asteroide es un objeto muerto cuando se inicia' do
+  it 'deberia ser un objeto sin vida al crearse ya que su vida y masa es 0' do
     asteroide = Asteroide.new 0,0
+    
     expect(asteroide.objeto_esta_vivo).to eq false
   end
 
-  it 'El objeto Asteroide se inicializa con masa de 1 ' do
+  it 'deberia contener 1 unidad de masa al crear el Asteroide' do
     asteroide = Asteroide.new 2, 1
+    
     expect(asteroide.masa).to eq 1
   end
 
-  it 'El objeto Asteroide se inicializa con vida de 0 ' do
+  it 'deberia contener 0 unidades de vida al crear el Asteroide' do
     asteroide = Asteroide.new 0,0
+    
     expect(asteroide.vida).to eq 0
   end
 
-  it 'El ingreso de parametros es incorrecto' do
- 	expect{asteroide = Asteroide.new 0,1,0}.to raise_error(ArgumentError)  
+  it 'deberia no permitir el ingreso de mas de dos parametros al crear el objeto espacial' do
+ 	  
+    expect{asteroide = Asteroide.new 1,1,1}.to raise_error(ArgumentError)  
+  end
+
+  it 'deberia contener 600 unidades de masa al impactar con otro objeto espacial del tipo Nave' do
+    asteroide = Asteroide.new 500, 500
+    nave = Nave.new 1000,1000
+
+    asteroide.impacta_con_objeto nave
+
+    expect(asteroide.masa).to eq 600
+    expect(asteroide.objeto_esta_vivo).to eq true
   end
 
 end
