14d05856759dd9878e86cd098b5c88db83d3ee6e
Emiliano <emilianoveppo@gmail.com>
2016-06-26 20:27:38 -0300

    Se realiza modificacion en test ya que eran poco intuitivos

--- spec/objeto_espacial_spec.rb
+++ spec/objeto_espacial_spec.rb
@@ -4,49 +4,49 @@ require_relative '../model/objeto_espacial'
 
 describe 'ObjetoEspacial' do
 
-  it 'Se inicia el Objeto Espacial con Vida 100' do
+  it 'deberia contener 100 unidades de vida al crear el objeto espacial' do
     objetoEspacial = ObjetoEspacial.new
     expect(objetoEspacial.vida).to eq 100
   end
 
-  it 'Se inicia el Objeto Espacial con Masa 100' do
+  it 'deberia contener 100 unidades de masa al crear el objeto espacial' do
     objetoEspacial = ObjetoEspacial.new
     expect(objetoEspacial.masa).to eq 100
   end
 
-   it 'El objeto espacial es un objeto vivo cuando se inicia' do
+  it 'deberia ser un objeto con vida al instanciarse' do
     objetoEspacial = ObjetoEspacial.new
     expect(objetoEspacial.objeto_esta_vivo).to eq true
   end
 
-   it 'El objeto espacial es un objeto muerto cuando su masa es 0' do
+  it 'deberia ser un objeto sin vida al contener su masa con valor 0' do
     objetoEspacial = ObjetoEspacial.new
     objetoEspacial.masa = 0
     expect(objetoEspacial.objeto_esta_vivo).to eq false
   end
 
-   it 'El objeto espacial es un objeto muerto cuando su vida es 0' do
+  it 'deberia ser un objeto sin vida al contener su vida con valor 0' do
     objetoEspacial = ObjetoEspacial.new
     objetoEspacial.vida = 0
     expect(objetoEspacial.objeto_esta_vivo).to eq false
   end
 
-   it 'El objeto espacial con masa de 300 ' do
+  it 'deberia contener 300 unidades de masa al crear el objeto espacial' do
     objetoEspacial = ObjetoEspacial.new 10, 300
     expect(objetoEspacial.masa).to eq 300
   end
 
-   it 'El objeto espacial vida de 1000 ' do
+  it 'deberia contener 1000 unidades de vida al crear el objeto espacial' do
     objetoEspacial = ObjetoEspacial.new 1000, 41
     expect(objetoEspacial.vida).to eq 1000
   end
    
-   it 'El objeto espacial tiene 1000 de vida y su estado es con vida' do
+  it 'deberia contener 1000 unidades de vida y ser un objeto vivo al crear el objeto espacial' do
     objetoEspacial = ObjetoEspacial.new 1000, 41
     expect(objetoEspacial.objeto_esta_vivo).to eq true
   end
 
-   it 'El ingreso de parametros es incorrecto' do
- 	  expect{objetoEspacial = ObjetoEspacial.new 1000, 41 , 10}.to raise_error(ArgumentError)  
+  it 'deberia no permitir el ingreso de mas de dos parametros al crear el objeto espacial' do
+ 	  expect{objetoEspacial = ObjetoEspacial.new 1000, 41 , 100, 10, 10}.to raise_error(ArgumentError)  
   end
 end
