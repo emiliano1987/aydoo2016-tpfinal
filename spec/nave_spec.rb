482cfd5b82d49314607af876de3b940c7ec6a97d
Emiliano <emilianoveppo@gmail.com>
2016-06-26 18:53:38 -0300

    Se agregan modificaciones en el objeto espacial que permitan el impacto entre diferentes objetos

--- spec/nave_spec.rb
+++ spec/nave_spec.rb
@@ -4,33 +4,43 @@ require_relative '../model/nave'
 
 describe 'Nave' do
 
-  it 'Se inicia Nave con Vida 100' do
+  it 'deberia contener 100 unidades de vida al crear la Nave' do
     nave = Nave.new
     expect(nave.vida).to eq 100
   end
 
-  it 'Se inicia Nave con Masa 100' do
+  it 'deberia contener 100 unidades de masa al crear la Nave' do
     nave = Nave.new
     expect(nave.vida).to eq 100
   end
 
-  it 'La nave es un objeto vivo cuando se inicia' do
+  it 'deberia esta viva la nave al generarse' do
     nave = Nave.new
     expect(nave.objeto_esta_vivo).to eq true
   end
 
-  it 'El objeto espacial con masa de 1 ' do
+  it 'deberia contener 1 unidad de masa al instanciarse' do
     nave = Nave.new 3000, 1
     expect(nave.masa).to eq 1
   end
 
-  it 'El objeto espacial vida de 4000 ' do
+  it 'deberia contener 4000 unidad de vida al crearse' do
     nave = Nave.new 4000, 8
     expect(nave.vida).to eq 4000
   end
 
-  it 'El ingreso de parametros es incorrecto' do
- 	expect{nave = Nave.new 1 }.to raise_error(ArgumentError)  
+  it 'deberia no permitir el ingreso de mas de dos parametros al crear la Nave' do
+ 	  expect{nave = Nave.new 1 }.to raise_error(ArgumentError)  
+  end
+
+  it 'deberia contener 8 unidades de vida al impactar con otro objeto espacial del mismo tipo (Nave)' do
+    nave = Nave.new 108, 10
+    nueva_nave = Nave.new
+
+    nave.impacta_con_objeto nueva_nave
+
+    expect(nave.vida).to eq 8
+    expect(nave.objeto_esta_vivo).to eq true
   end
 
 end
